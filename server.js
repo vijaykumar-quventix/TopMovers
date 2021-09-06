49cd633a9e9c747cc3f9bf80202675a2bbced14c
Vijay Kumar <vijay.kumar@quventix.com>
2021-09-06 20:00:18 +0530

    Working On Project Setup

--- server.js
+++ server.js
@@ -4,15 +4,32 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const mysqlConnection = require('./config/database');
-const customer_routes = require('./routes/customerRoutes');
-const driver_routes = require('./routes/driverRoutes');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 mysqlConnection.mysqlConnection();
 
 const port = process.env.PORT || 3000;
 
+// Importing Routes
+const customer_routes = require('./routes/customerRoutes');
+const driver_routes = require('./routes/driverRoutes');
+
+// Use Routes
 app.use('/customer', customer_routes);
 app.use('/driver', driver_routes);
+
+// swagger setup
+const swaggerDefinition = require('./lib/swaggerdefinition');
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 
 app.listen(port, () => console.log(`Server Running On Port ${port}`));
